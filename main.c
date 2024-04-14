0bafd7aa2e6b32b9b94cc1845b9510277c41854c
yoswuk <yoswuk@korea.ac.kr>
2024-04-14 20:47:39 +0900

    commit

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+//CIPHER()
+
+struct state {
+    
+}
+int CIPHER()
+{
+    for (int i = 0; i < Nr; i++)
+    {
+        state = SUBBYTES(state);
+        state = SHIFTROWS(state);
+        state = MIXCOLUMNS(state);
+        state = ADDROUNDKEY(state, w + 4 * i)
+    }
+
+    state = SUBBYTES(state);
+    state = SHIFTROWS(state);
+    state =ADDROUNDKEY(state, w + 4 * Nr);
+
+    return state;98
+}
+
 int SUBBYTES()
 {
 
@@ -9,6 +31,36 @@ int SHIFTROWS()
 {
 
 }
+
+int MIXCOLUMNS()
+{
+
+}
+
+int ADDROUNDKEY()
+{
+
+}
+
+//KEYEXPANSION()
+
+//INVCIPHER()
+int INVSHIFTROWS()
+{
+
+}
+
+int INVSUBBYTES()
+{
+
+}
+
+int INVMIXCOLUMNS()
+{
+
+}
+
+
 int main(void)
 {
     return 0;
